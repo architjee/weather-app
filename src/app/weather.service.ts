943bcb5a2930eff765a2b4583475159d01fa235e
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-11-17 19:02:24 +0530

    Adding getWeather method in weather service

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,5 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Weather } from './weather';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +11,14 @@ export class WeatherService {
 
   constructor(private http: HttpClient) {
 
+   }
+
+   getWeather(city: string) : Observable<Weather> {
+    const options = new HttpParams()
+    .set('units', 'metric')
+    .set('q', city)
+    .set('appId', environment.apiKey)
+
+    return this.http.get<Weather>(environment.apiUrl+'weather', {params: options});
    }
 }
